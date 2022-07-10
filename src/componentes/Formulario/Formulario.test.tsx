eba8421af3f62979364aae8fd1dd01598413c989
unknown <nataliamartineli@gmail.com>
2022-07-10 11:27:38 -0300

    Created tests to prevent more than 2 users with the same name; Created test to hide error message; Organized code

--- src/componentes/Formulario/Formulario.test.tsx
+++ src/componentes/Formulario/Formulario.test.tsx
@@ -1,20 +1,124 @@
-import { render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import Formulario from './Formulario';
 
 //Jest - O teste acaba servindo como documentação de como o componente tem que funcionar
+describe('o comportamento do Formulario.tsx', () => {
+	test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
+		render(
+			<RecoilRoot>
+				<Formulario />
+			</RecoilRoot>
+		);
+		//encontrar no dom o input
+		const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+		//encontrar o botão
+		const button = screen.getByRole('button');
+		//garantir que o input esteja no documento - JEST
+		expect(input).toBeInTheDocument();
+		//garantir que o botão esteja desabilitado - JEST
+		expect(button).toBeDisabled();
+	});
 
-test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
-	render(<Formulario />);
-	//encontrar no dom o input
-	const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+	test('adicionar um participante caso exista um nome preenchido', () => {
+		render(
+			<RecoilRoot>
+				<Formulario />
+			</RecoilRoot>
+		);
 
-	//encontrar o botão
-	const button = screen.getByRole('button');
+		//encontrar no dom o input
+		const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+		//encontrar o botão
+		const button = screen.getByRole('button');
 
-	//garantir que o input esteja no documento - JEST
-	expect(input).toBeInTheDocument();
+		//inserir um valor no input
+		fireEvent.change(input, {
+			target: {
+				value: 'Marília',
+			},
+		});
+		//clicar no botão de submeter
+		fireEvent.click(button);
+		//garantir que o input esteja com o foco ativo
+		expect(input).toHaveFocus();
+		//garantir que o input não tenha um valor
+		expect(input).toHaveValue('');
+	});
 
-	//garantir que o botão esteja desabilitado - JEST
-	expect(button).toBeDisabled();
+	test('nomes duplicados não podem ser adicionados à lista', () => {
+		render(
+			<RecoilRoot>
+				<Formulario />
+			</RecoilRoot>
+		);
+
+		const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+		const button = screen.getByRole('button');
+
+		//disparar duas vezes o input com o mesmo valor
+		fireEvent.change(input, {
+			target: {
+				value: 'Marília',
+			},
+		});
+		fireEvent.click(button);
+
+		fireEvent.change(input, {
+			target: {
+				value: 'Marília',
+			},
+		});
+		fireEvent.click(button);
+
+		//procurar por um alerta
+		const mensagemDeErro = screen.getByRole('alert');
+		//encontrar uma mensagem de erro com o texto definido
+		expect(mensagemDeErro.textContent).toBe('Nomes duplicados não são permitidos');
+	});
+
+	test('a mensagem de erro deve sumir após os timers', () => {
+		jest.useFakeTimers();
+
+		render(
+			<RecoilRoot>
+				<Formulario />
+			</RecoilRoot>
+		);
+
+		const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+		const button = screen.getByRole('button');
+
+		//disparar duas vezes o input com o mesmo valor
+		fireEvent.change(input, {
+			target: {
+				value: 'Marília',
+			},
+		});
+		fireEvent.click(button);
+
+		fireEvent.change(input, {
+			target: {
+				value: 'Marília',
+			},
+		});
+		fireEvent.click(button);
+
+		//procurar por um alerta
+		//query by role: quando vamos no screen e fazemos um método get, se ele não encontrar o teste vai falhar.
+		//Se é ok não encontrar o elemento, usamos o queryByRole
+		let mensagemDeErro = screen.queryByRole('alert');
+		expect(mensagemDeErro).toBeInTheDocument();
+
+		//esperar N segundos - fala para o jest usar os timers disponíveis
+
+		act(() => {
+			jest.runAllTimers();
+		});
+
+		//mensagem deve ser nula
+		mensagemDeErro = screen.queryByRole('alert');
+		expect(mensagemDeErro).toBeNull();
+	});
 });
--- src/componentes/Formulario/Formulario.tsx
+++ src/componentes/Formulario/Formulario.tsx
@@ -1,8 +1,31 @@
+import { useRef, useState } from 'react';
+import { useAdicionarParticipante } from '../../state/hooks/useAdicionarParticipante';
+import { useMensagemDeErro } from '../../state/hooks/useMensagemDeErro';
+
 const Formulario = () => {
+	const [nome, setNome] = useState('');
+	const inputRef = useRef<HTMLInputElement>(null);
+	const adicionarNaLista = useAdicionarParticipante();
+	const mensagemDeErro = useMensagemDeErro();
+
+	const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+		evento.preventDefault();
+		adicionarNaLista(nome);
+		setNome('');
+		inputRef.current?.focus();
+	};
+
 	return (
-		<form>
-			<input type="text" placeholder="Insira os nomes dos participantes" />
-			<button disabled={true}>Adicionar</button>
+		<form onSubmit={adicionarParticipante}>
+			<input
+				ref={inputRef}
+				value={nome}
+				onChange={(evento) => setNome(evento.target.value)}
+				type="text"
+				placeholder="Insira os nomes dos participantes"
+			/>
+			<button disabled={!nome}>Adicionar</button>
+			{mensagemDeErro && <p role="alert">{mensagemDeErro}</p>}
 		</form>
 	);
 };
