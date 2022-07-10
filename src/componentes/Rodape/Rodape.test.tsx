4cc05578c0e39f63ff660d2258eda32169c03a6b
unknown <nataliamartineli@gmail.com>
2022-07-10 19:19:36 -0300

    Created Config page and snapshot test

--- src/componentes/Rodape/Rodape.test.tsx
+++ src/componentes/Rodape/Rodape.test.tsx
@@ -1,9 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import { useListaParticipantes } from '../../state/hooks/useListaParticipantes';
 import Rodape from './Rodape';
 
-describe('onde não existem participantes suficientes', () => {
+//mock da lista de participantes
+jest.mock('../../state/hooks/useListaParticipantes', () => {
+	return {
+		useListaParticipantes: jest.fn(),
+	};
+});
+
+const mockNavegacao = jest.fn();
+
+jest.mock('react-router-dom', () => {
+	return {
+		useNavigate: () => mockNavegacao,
+	};
+});
+
+describe('quando não existem participantes suficientes', () => {
+	beforeEach(() => {
+		(useListaParticipantes as jest.Mock).mockReturnValue([]);
+	});
 	test('a brincadeira não pode ser iniciada', () => {
 		render(
 			<RecoilRoot>
@@ -13,5 +32,35 @@ describe('onde não existem participantes suficientes', () => {
 
 		const button = screen.getByRole('button');
 		expect(button).toBeDisabled();
+	});
+});
+
+describe('quando existem participantes suficientes', () => {
+	beforeEach(() => {
+		(useListaParticipantes as jest.Mock).mockReturnValue(['Marisa', 'Ana', 'Silvio']);
+	});
+	test('a brincadeira pode ser iniciada', () => {
+		render(
+			<RecoilRoot>
+				<Rodape />
+			</RecoilRoot>
+		);
+
+		const button = screen.getByRole('button');
+		expect(button).not.toBeDisabled();
+	});
+
+	test('a brincadeira foi iniciada', () => {
+		render(
+			<RecoilRoot>
+				<Rodape />
+			</RecoilRoot>
+		);
+
+		const button = screen.getByRole('button');
+		fireEvent.click(button);
+
+		expect(mockNavegacao).toHaveBeenCalledTimes(1);
+		expect(mockNavegacao).toHaveBeenCalledWith('/sorteio');
 	});
 });
--- src/componentes/Rodape/Rodape.tsx
+++ src/componentes/Rodape/Rodape.tsx
@@ -3,11 +3,17 @@ import { useListaParticipantes } from '../../state/hooks/useListaParticipantes';
 
 const Rodape = () => {
 	const participantes = useListaParticipantes();
-	//const navegarPara = useNavigate
+	const navegarPara = useNavigate();
+
+	const iniciar = () => {
+		navegarPara('/sorteio');
+	};
 
 	return (
 		<footer>
-			<button disabled={participantes.length < 3}>Iniciar brincadeira</button>
+			<button disabled={participantes.length < 3} onClick={iniciar}>
+				Iniciar brincadeira
+			</button>
 		</footer>
 	);
 };
